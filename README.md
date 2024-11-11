e09386c6a4ea122d85081ded52d6780ad25a38c3
Lukas <me@lhertkorn.de>
2024-11-11 15:08:44 +0100

    started mesh implementation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SpheresCollisionDetectionStudien
 Studienarbeit Kollisions-Erkennung auf Basis von Kugeln die Shapes approximieren.
+
+## Dependencies
+Dieses Projekt verwendet aktuell Forks der Bibliotheken:
+- [JURDF](https://github.com/lukashert/jurdf)
+- [EuclidView3d](https://github.com/lukashert/EuclidView3d)
+
+Nach Fertigstellung dieses Projektes sollten die Änderungen gemergt werden.
--- src/main/java/de/dhbw/visualizer/Main.java
+++ src/main/java/de/dhbw/visualizer/Main.java
@@ -1,20 +1,39 @@
 package de.dhbw.visualizer;
 
 import de.orat.math.view.euclidview3d.GeometryView3d;
+import de.orat.math.xml.urdf.Visualizer;
+import de.orat.math.xml.urdf.api.Chain;
+import de.orat.math.xml.urdf.api.Urdf;
 import de.orat.view3d.euclid3dviewapi.api.ViewerService;
+import org.jogamp.vecmath.Matrix4d;
 import org.jogamp.vecmath.Point3d;
-import org.jogamp.vecmath.Vector3d;
 
 import java.awt.*;
 
 public class Main extends GeometryView3d {
 
     public static void main(String[] args) throws Exception {
-        ViewerService viewerService = ViewerService.getInstance();
+        /*ViewerService viewerService = ViewerService.getInstance();
         var viewer = viewerService.getViewer().orElseThrow();
         viewer.open();
 
         viewer.addSphere(new Point3d(1, 1, 1), 3, Color.RED, "Test", false);
         viewer.addSphere(new Point3d(1, 1, 1), 4, Color.RED, "Test", true);
+*/
+
+        Visualizer visualizer = new Visualizer();
+
+        if (visualizer.getViewer() != null){
+           // Urdf jurdf = new Urdf(visualizer.getClass().getResourceAsStream("ur5eA.urdf"));
+            //Chain chain = jurdf.createChain();
+
+            visualizer.getViewer().open();
+            visualizer.getViewer().addMesh("/data/objfiles/base.dae",new Matrix4d());
+
+            //visualizer.add(chain);
+
+        } else {
+            System.out.println("No Visualizer implementation found!");
+        }
     }
 }
